5dbf47462b573bb3c27cbf92397f926d11a0eb36
parvez1407 <info.nahidwd@gmail.com>
2022-11-28 20:25:38 +0600

    jwt token implement and final commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const port = process.env.PORT || 5000;
@@ -17,6 +18,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
+
+
+
 async function run() {
     try {
         const usersCollections = client.db('carHut').collection('users');
@@ -26,6 +45,18 @@ async function run() {
         const wishlistsCollection = client.db('carHut').collection('wishlists');
         const paymentsCollection = client.db('carHut').collection('payments');
         const promotionsCollection = client.db('carHut').collection('promotions');
+
+        // jwt token
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollections.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '7d' });
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        })
 
         // user collection api
         app.put('/user/:email', async (req, res) => {
@@ -74,7 +105,7 @@ async function run() {
 
 
         // payments collection
-        app.post('/payments', async (req, res) => {
+        app.post('/payments', verifyJWT, async (req, res) => {
             const payment = req.body;
             const result = await paymentsCollection.insertOne(payment);
             const id = payment.bookingId;
@@ -91,7 +122,7 @@ async function run() {
             res.send(result);
         })
         // seller verification
-        app.post('/verification', async (req, res) => {
+        app.post('/verification', verifyJWT, async (req, res) => {
             const verification = req.body;
             const email = verification.email;
             const filter = { sellerEmail: email };
@@ -105,22 +136,20 @@ async function run() {
 
         })
 
-
-
         // get sellers and buyers
-        app.get('/sellers', async (req, res) => {
+        app.get('/sellers', verifyJWT, async (req, res) => {
             const result = await usersCollections.find({ role: 'seller' }).toArray();
             res.send(result)
         })
         // delete single seller
-        app.delete('/sellers/:id', async (req, res) => {
+        app.delete('/sellers/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const result = await usersCollections.deleteOne(filter);
             res.send(result);
         })
         // delete single buyers
-        app.delete('/buyers/:id', async (req, res) => {
+        app.delete('/buyers/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const result = await usersCollections.deleteOne(filter);
@@ -130,7 +159,7 @@ async function run() {
 
 
         // get buyers and buyers
-        app.get('/buyers', async (req, res) => {
+        app.get('/buyers', verifyJWT, async (req, res) => {
             const result = await usersCollections.find({ role: 'buyer' }).toArray();
             res.send(result)
         })
@@ -141,14 +170,14 @@ async function run() {
             res.send(result);
         })
         // post product objects to database
-        app.post('/products', async (req, res) => {
+        app.post('/products', verifyJWT, async (req, res) => {
             const product = req.body;
             const result = await productsCollection.insertOne(product);
             res.send(result);
         })
 
         // post advertises collection
-        app.post('/promotions', async (req, res) => {
+        app.post('/promotions', verifyJWT, async (req, res) => {
             const advertise = req.body;
             const result = await promotionsCollection.insertOne(advertise);
             res.send(result);
@@ -161,7 +190,7 @@ async function run() {
         })
 
         // delete product by seller
-        app.delete('/products/:id', async (req, res) => {
+        app.delete('/products/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const result = await productsCollection.deleteOne(filter);
@@ -177,7 +206,7 @@ async function run() {
             res.send(categoryProduct);
         });
         // get specific product collection of seller
-        app.get('/myproducts/:email', async (req, res) => {
+        app.get('/myproducts/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { sellerEmail: email }
             const result = await productsCollection.find(query).toArray();
@@ -185,14 +214,14 @@ async function run() {
         })
 
         // post bookings collection
-        app.post('/bookings', async (req, res) => {
+        app.post('/bookings', verifyJWT, async (req, res) => {
             const booking = req.body;
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
 
         // get bookings data by email for specific buyer
-        app.get('/bookings/:email', async (req, res) => {
+        app.get('/bookings/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { buyerEmail: email };
             const result = await bookingsCollection.find(query).toArray();
@@ -208,13 +237,13 @@ async function run() {
         })
 
         // post wishlist collection
-        app.post('/wishlists', async (req, res) => {
+        app.put('/wishlists', verifyJWT, async (req, res) => {
             const wishlist = req.body;
             const result = await wishlistsCollection.insertOne(wishlist);
             res.send(result);
         })
         // get wishlist data by email for specific buyer
-        app.get('/wishlists/:email', async (req, res) => {
+        app.get('/wishlists/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { buyerEmail: email };
             const result = await wishlistsCollection.find(query).toArray();
